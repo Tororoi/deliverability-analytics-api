e4ad110c66be1a9dd191c3c112a01b2d98b65501
Tororoi <cantwell.tom@gmail.com>
2021-02-23 17:31:56 -0500

    adding controller methods

--- app/controllers/api/devices_controller.rb
+++ app/controllers/api/devices_controller.rb
@@ -1,2 +1,35 @@
 class Api::DevicesController < ApplicationController
+
+    def register
+        # Create a new device in the database
+        @device = Device.create(device_params)
+    end
+
+    def alive
+        # Create a new heartbeat associated with device
+        @device = Device.find(params[:id])
+        @heartbeat = @device.heartbeats.create()
+    end
+
+    def report
+        # Create a new report associated with device
+        @device = Device.find(params[:id])
+        @report = @device.reports.create()
+    end
+
+    def terminate
+        # Update device as disabled with timestamp
+        @device = Device.find(params[:id])
+        # assign current datetime to @device[:disabled_at]
+    end
+
+    private
+
+    def device_params
+        params.permit(:phone_number, :carrier)
+    end
+
+    def report_params
+        params.require(:device).permit(:message, :sender)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@
 
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/api' do
-    # Create a new device in the database
     post 'register', to: 'devices#register'
-    # Create a new heartbeat associated with device
     post 'alive', to: 'devices#alive'
-    # Create a new report associated with device
     post 'report', to: 'devices#report'
-    # Update device as disabled with timestamp
     patch 'terminate', to: 'devices#terminate'
   end
 end
