f58b44ae605af3b12b6be795dd724595b365a166
Tororoi <cantwell.tom@gmail.com>
2021-02-23 12:32:30 -0500

    added routes and api namespace

---
+++ app/controllers/api/devices_controller.rb
@@ -0,0 +1,2 @@
+class Api::DevicesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/api' do
+    # Create a new device in the database
+    post 'register', to: 'devices#register'
+    # Create a new heartbeat associated with device
+    post 'alive', to: 'devices#alive'
+    # Create a new report associated with device
+    post 'report', to: 'devices#report'
+    # Update device as disabled with timestamp
+    patch 'terminate', to: 'devices#terminate'
+  end
 end
